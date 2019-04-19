68c0c4b32e9dead9ed495e580b673ed448a10bf7
Max <maxinekho@nyu.edu>
2019-04-18 20:37:45 -0400

    v1.2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,18 @@
 class ProjectsController < ApplicationController
-    def janus
-    end
-    def treasury
-    end
-    def opentable
-    end
-    def pizza
-    end
-    def ice
-    end
-    def fortunecookie
-    end
-    def luckycat
-    end
-    def disclosure
-    end
+    # def janus
+    # end
+    # def treasury
+    # end
+    # def opentable
+    # end
+    # def pizza
+    # end
+    # def ice
+    # end
+    # def fortunecookie
+    # end
+    # def luckycat
+    # end
+    # def disclosure
+    # end
 end
